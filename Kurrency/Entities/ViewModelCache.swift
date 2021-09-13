ff4d41eedae4ba8c367cb40c884cc3ffbf6b72aa
Nugroho Arief Widodo <nawidodo@gmail.com>
2021-09-13 13:40:32 +0700

    Stable 1.3

---
+++ Kurrency/Entities/ViewModelCache.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelCache.swift
+//  Kurrency
+//
+//  Created by Nugroho Arief Widodo on 13/09/21.
+//
+
+import Foundation
---
+++ Kurrency/Extensions/UserDefaults+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaults+Extensions.swift
+//  Kurrency
+//
+//  Created by Nugroho Arief Widodo on 13/09/21.
+//
+
+import Foundation
